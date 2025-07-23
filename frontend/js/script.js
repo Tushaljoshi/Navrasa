939b24c2e8dd99db5b36949e2d8d9b6cc69f4d73
Tushaljoshi <joshitushal1@gmail.com>
2025-07-23 18:14:29 +0530

    frontend UI Updates

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -432,6 +432,22 @@ function typeEffect() {
 
 typeEffect();
 
+/* booton to top scroller*/
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  };
+
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+
 
 
 
